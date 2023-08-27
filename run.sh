0ca7c08c518c807c93058a8e141f2b371109a9a4
Binh Ong <binhot@pm.me>
2023-08-27 13:12:17 +0700

    fix prod

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-export FLASK_ENV=production && export FLASK_APP=src/umtis/app && poetry run python -m flask run 
+export FLASK_ENV=production && export FLASK_APP=src/umtis/app && poetry shell && python3 src/umtis/app.py
