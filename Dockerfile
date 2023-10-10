40fa0f182488039def8799b2edfd61b9ce06db79
Binh Ong <binhot@pm.me>
2023-10-10 13:26:46 +0700

    update

--- Dockerfile
+++ Dockerfile
@@ -12,41 +12,7 @@ RUN apt-get install -y \
     curl \
     gnupg \
     wget 
-# RUN apt-get install -y \
-#     fonts-liberation \
-#     libasound2 \
-#     libatk-bridge2.0-0 \
-#     libatk1.0-0 \
-#     libatspi2.0-0 \
-#     libcups2 \
-#     libdbus-1-3 \
-#     libdrm2 \
-#     libgbm1 \
-#     libgtk-3-0 \
-# #    libgtk-4-1 \
-#     libnspr4 \
-#     libnss3 \
-#     libwayland-client0 \
-#     libxcomposite1 \
-#     libxdamage1 \
-#     libxfixes3 \
-#     libxkbcommon0 \
-#     libxrandr2 \
-#     xdg-utils \
-#     libu2f-udev \
-#     libvulkan1
 
-# # Update and install necessary packages
-
-# # Install Google Chrome
-
-# RUN curl -LO  https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_114.0.5735.90-1_amd64.deb
-# RUN apt-get install -y ./google-chrome-stable_114.0.5735.90-1_amd64.deb
-
-# # RUN sleep 6000;cqlsh -f build_all.cql
-# RUN rm google-chrome-stable_114.0.5735.90-1_amd64.deb
-
-# RUN echo "Chrome: " && google-chrome --version
 
 
 RUN curl -sSL https://dl.google.com/linux/linux_signing_key.pub | apt-key add - && \
--- src/umtis/app.py
+++ src/umtis/app.py
@@ -1,75 +1,63 @@
-from flask import Flask, render_template, request
-import json
-import time
-import os
-import random
-import undetected_chromedriver as uc
-import requests
-
+from flask import Flask, request, jsonify
 from seleniumwire import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import StaleElementReferenceException
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions
-from concurrent import futures
-
+from selenium.webdriver.support import expected_conditions as EC
 from umtis.fetch import main as fetch_main
 from umtis.seleniumpro.local_storage import LocalStorage
 import random
+import undetected_chromedriver as uc
+import requests
+import time
+import os
+import json
+from concurrent import futures
 from umtis import log
 
 logger = log.setup_logging("SIS")
 
 app = Flask(__name__)
 
+# Constants
+LOGIN_TIMEOUT = 10
+WAIT_SHORT = 0.3
+WAIT_MEDIUM = 0.5
+WAIT_LONG = 3
 
-def fill_input_xpath(driver: webdriver, xpath_value: str, value: str, ):
-    WebDriverWait(driver, 10).until(expected_conditions.element_to_be_clickable((By.XPATH, xpath_value))).send_keys(value)
-
-
-
-def click_button_xpath(driver: webdriver, xpath_value: str, id=-1):
-    WebDriverWait(driver, 10).until(expected_conditions.element_to_be_clickable((By.XPATH, xpath_value))).click()
-
-
-def soft_click_button_xpath(driver: webdriver, xpath_value: str, id=-1):
-    WebDriverWait(driver, 4).until(expected_conditions.element_to_be_clickable((By.XPATH, xpath_value))).click()
-
-def click_if_found(driver: webdriver, xpath_value: str):
-    sleep_and_wait(0.3)
-    if len(driver.find_elements(By.XPATH, xpath_value)) > 0:
-        driver.find_element(By.XPATH, xpath_value).click()
-def find_exist_xpath(driver: webdriver, xpath_value: str):
-    sleep_and_wait(0.1)
-
-    
-    if len(driver.find_elements(By.XPATH, xpath_value)) < 1:
-        sleep_and_wait(0.2)
-
-    return len(driver.find_elements(By.XPATH, xpath_value))
-
-
-def sleep_and_wait(seconds: int):
+def sleep_and_wait(seconds):
     time.sleep(seconds)
-
-
-def get_random_string(length):
     
-    result_str = ''.join(random.choice(string.ascii_letters)
-                         for i in range(length))
+def wait_and_click(driver, xpath_value, timeout=LOGIN_TIMEOUT):
+    WebDriverWait(driver, timeout).until(EC.element_to_be_clickable((By.XPATH, xpath_value))).click()
+
+def wait_and_send_keys(driver, xpath_value, value, timeout=LOGIN_TIMEOUT):
+    WebDriverWait(driver, timeout).until(EC.element_to_be_clickable((By.XPATH, xpath_value))).send_keys(value)
+
+def find_element(driver, xpath_value):
+    return len(driver.find_elements(By.XPATH, xpath_value)) > 0
+
+def click_if_found(driver, xpath_value):
+    logger.info(f"Checking if element exists: {xpath_value}")
+    sleep_and_wait(WAIT_SHORT)
+    if find_element(driver, xpath_value):
+        logger.info(f"Clicking on element: {xpath_value}")
+        driver.find_element(By.XPATH, xpath_value).click()
+
+def login_successful(driver):
+    return EC.url_contains("office.com")(driver)
+
+def fetch_access_token(driver):
+    lcs = LocalStorage(driver)
+    user = json.loads(lcs.get("sis-auth-react"))
+    uid = user['user']['uid']
+    pid = user['user']['id']
+    access_token = user['token']
     
-    return result_str
+    return access_token
 
 
-@app.route('/')
-def ping():
-    return 'pong'
-
-
-def performance_login(user_profile, id):
+def perform_login(user_profile, request_id):
     options = uc.ChromeOptions()
     prefs = {"profile.password_manager_enabled": False, "credentials_enable_service": False, "useAutomationExtension": False}
     options.add_experimental_option("prefs", prefs)
@@ -86,92 +74,84 @@ def performance_login(user_profile, id):
     options.add_argument("--disable-popup-blocking")
     options.add_argument("--disable-notifications")
     options.add_argument("--disable-popup-blocking")
+    options.add_argument('--dns-prefetch-disable')  # Disable DNS prefetching
+    options.add_argument('--dns-server=8.8.8.8')
+
     options.add_argument('--ignore-ssl-errors=yes')
     options.add_argument('--ignore-certificate-errors')
     options.add_argument("--clear-data")
     options.add_argument("--incognito")
     options.add_argument("--disable-blink-features=AutomationControlled")
-    logger.info(f"[{id}] Fire up")
-    driver = uc.Chrome(options=options, headless=True)
+    
+    logger.info(f"[{request_id}] Initiating login process")
+    
+    driver = uc.Chrome(options=options, headless=False)
     driver.get("https://login.microsoftonline.com/")
-    logger.info(f"[{id}] Go to office.com")
-    fill_input_xpath(driver, "//input[@id='i0116']", user_profile['username'])
-    
-    click_button_xpath(driver, '//input[@id="idSIButton9"]', id)
-    is_failed = find_exist_xpath(driver, '//div[@id="usernameError"]')
-    if is_failed > 0:
-        logger.error(f"[{id}] Login failed: Wrong Username")
 
+    logger.info(f"[{request_id}] Waiting for login page to load")
+    wait_and_send_keys(driver, "//input[@id='i0116']", user_profile['username'])
+    logger.info(f"[{request_id}] Filled username")
+
+    wait_and_click(driver, '//input[@id="idSIButton9"]', request_id)
+
+    if find_element(driver, '//div[@id="usernameError"]'):
         driver.close()
+        logger.error(f"[{request_id}] Login failed: Wrong Username")
         return {"error": True, "msg": "WRONG_USERNAME"}
-    logger.info(f"[{id}] Not-error at Input email")
-    fill_input_xpath(driver, '//input[@id="i0118"]', user_profile['password'])
-    logger.info(f"[{id}] Input password done")
 
-    click_button_xpath(driver, '//input[@id="idSIButton9"]', id)
+    wait_and_send_keys(driver, '//input[@id="i0118"]', user_profile['password'])
+    logger.info(f"[{request_id}] Filled password")
 
-    
-    
+    wait_and_click(driver, '//input[@id="idSIButton9"]', request_id)
 
-    logger.info(f"[{id}] Login")
-
-    is_failed = find_exist_xpath(driver, '//div[@id="passwordError"]')
-    if is_failed > 0:
-        logger.error(f"[{id}] Login failed: Wrong password")
+    if find_element(driver, '//div[@id="passwordError"]'):
         driver.close()
-
+        logger.error(f"[{request_id}] Login failed: Wrong password")
         return {"error": True, "msg": "WRONG_PASSWORD"}
-        
-    logger.info(f"[{id}] Password success")
+
+    logger.info(f"[{request_id}] Confirming login")
+
     click_if_found(driver, '//input[@id="idSIButton9"]')
-    logger.info(f"[{id}] Click success")
+    WebDriverWait(driver, LOGIN_TIMEOUT).until(login_successful)
+    logger.info(f"[{request_id}] SIS Login")
 
-    WebDriverWait(driver, 20).until(expected_conditions.url_contains("https://www.office.com/?auth=2"))
 
-    
-    cons = requests.get("https://apisis.umt.edu.vn/api/v1.0/auth2/authorize?role=STUDENT")
-    t = cons.text.replace('user.read%20offline_access%20openid%20profile','openid')
-    
-    driver.get(t)
-    
+    consent_response = requests.get("https://apisis.umt.edu.vn/api/v1.0/auth2/authorize?role=STUDENT")
+    consent_url = consent_response.text.replace('user.read%20offline_access%20openid%20profile', 'openid')
 
-    element = find_exist_xpath(driver, f"//div[@class='table'][@tabindex='0'][@role='button'][@data-test-id='{user_profile['username']}']")
-    if (element == 0):
-        logger.warn(f"[{id} Login failed. Try again")
+    driver.get(consent_url)
+    logger.info(f"[{request_id}] Microsoft consent page loaded")
+    WebDriverWait(driver, LOGIN_TIMEOUT).until(EC.visibility_of_element_located((By.XPATH, '//div[@class="row title ext-title"]')))
+    user_element_exists = find_element(driver, f"//div[@class='table'][@tabindex='0'][@role='button'][@data-test-id='{user_profile['username']}']")
+    if not user_element_exists:
+        logger.warning(f"[{request_id}] Login failed. Try again")
         driver.close()
         return {"error": True, "msg": "UNKNOWN_ERROR"}
     else:
-        logger.info(f"[{id}] Login click")
-        driver.get_screenshot_as_file(f"out/{id}-d1.png")
+        logger.info(f"[{request_id}] User element found, clicking login")
+        wait_and_click(driver, f"//div[@class='table'][@tabindex='0'][@role='button'][@data-test-id='{user_profile['username']}']", request_id)
+        logger.info(f"[{request_id}] Login click success")
+        WebDriverWait(driver, LOGIN_TIMEOUT).until(EC.visibility_of_element_located((By.XPATH, '//div[@class="row title ext-title"]')))
 
-        click_button_xpath(driver, f"//div[@class='table'][@tabindex='0'][@role='button'][@data-test-id='{user_profile['username']}']", id)
-        driver.get_screenshot_as_file(f"out/{id}-d2.png")
-        sleep_and_wait(3)
-        logger.info(f"[{id}] Login click success")
-        
-        click_if_found(driver, '//input[@id="idSIButton9"]')
-        driver.get_screenshot_as_file(f"out/{id}-d3.png")
+        wait_and_click(driver, '//input[@id="idSIButton9"]')
 
-    logger.info(f"[{id}] Wait for login success")
-
-    WebDriverWait(driver, 50).until(expected_conditions.url_contains("https://sis.umt.edu.vn/my-schedule"))
-
-    logger.info(f"[{id}] Done fetching token")
-    driver.get_screenshot_as_file(f"out/{id}-c8.png")
-
-    lcs = LocalStorage(driver)
-    user = json.loads(lcs.get("sis-auth-react"))
-    uid = user['user']['uid']
-    pid = user['user']['id']
-
-    access_token = user['token']
+    logger.info(f"[{request_id}] Waiting for login success")
+    WebDriverWait(driver, 50).until(EC.url_contains("https://sis.umt.edu.vn/my-schedule"))
+    logger.info("Load success! Fetch token! Last step")
+    # Rest of the login process here
+    access_token = fetch_access_token(driver)
 
     driver.close()
+
     if len(access_token) > 0:
-        return {"error": False, "token": access_token, "message": "SUCCESS", "suid": uid, 'puid': pid}
+        return {"error": False, "token": access_token, "message": "SUCCESS"}
     else:
         return {"error": True, "message": "UNKNOWN_ERROR"}
 
+
+@app.route('/')
+def ping():
+    return 'pong'
 
 @app.route('/login', methods=['POST'])
 def browser():
@@ -179,81 +159,21 @@ def browser():
     logger.info(f'[{nid}] New request')
 
     content_type = request.headers.get('Content-Type')
-    if (content_type == 'application/json'):
+    if content_type == 'application/json':
         user_profile = request.json
     else:
-        user_profile = {username: request.args.get(
-            "username"), password: request.args.get("password")}
-        
+        user_profile = {
+            "username": request.args.get("username"),
+            "password": request.args.get("password")
+        }
 
     try:
-        return_data = performance_login(user_profile, nid)
+        return_data = perform_login(user_profile, nid)
     except Exception as e:
-        logger.info(e)
+        logger.error(e)
         return {"error": True, "message": "UNKNOWN_ERROR"}
-    return return_data
-    
-    
 
+    return jsonify(return_data)
 
 if __name__ == '__main__':
-
-    # logger.info('Testing')
-    # options = uc.ChromeOptions()
-
-    # prefs = {"profile.password_manager_enabled": False, "credentials_enable_service": False, "useAutomationExtension": False}
-    # options.add_experimental_option("prefs", prefs)
-    # options.add_argument('-no-first-run')
-    # options.add_argument('-force-color-profile=srgb')
-    # options.add_argument('-metrics-recording-only')
-    # options.add_argument('-password-store=basic')
-    # options.add_argument('-use-mock-keychain')
-    # options.add_argument('-export-tagged-pdf')
-    # options.add_argument('-no-default-browser-check')
-    # options.add_argument('-disable-background-mode')
-    # options.add_argument('-enable-features=NetworkService,NetworkServiceInProcess,LoadCryptoTokenExtension,PermuteTLSExtensions')
-    # options.add_argument('-disable-features=FlashDeprecationWarning,EnablePasswordsAccountStorage')
-    # options.add_argument('-deny-permission-prompts')
-    # options.add_argument("--disable-popup-blocking")
-    # options.add_argument("--disable-notifications")
-    # options.add_argument("--disable-popup-blocking")
-    # options.add_argument('--ignore-ssl-errors=yes')
-    # options.add_argument('--ignore-certificate-errors')
-    # options.add_argument("--clear-data")
-    
-    # options.add_argument("--incognito")
-    # options.add_argument("--disable-blink-features=AutomationControlled")
-
-
-    # driver = uc.Chrome(options=options,headless=True)
-    # driver.get("https://www.google.com.vn/")
-    # js = '''
-    #     let callback = arguments[0];
-    #     let xhr = new XMLHttpRequest();
-    #     xhr.open('GET', 'https://www.google.com.vn/', true);
-    #     xhr.onload = function () {
-    #         if (this.readyState === 4) {
-    #             callback(this.status);
-    #         }
-    #     };
-    #     xhr.onerror = function (err) {
-    #         console.log(err);
-    #         callback('error');
-    #     };
-    #     xhr.send(null);
-    # '''
-    
-    # status_code = driver.execute_async_script(js)
-    
-    # if status_code in [200, 301, 302]:
-    #     logger.info("Seleium OK")
-    # else:
-    #     raise Exception("Selenium failed")
-    # driver.close()
     app.run(threaded=False, processes=8, host="0.0.0.0", port=os.getenv("PORT"))
-
-
-
-
-
-
